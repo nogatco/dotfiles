07c563abde27be29a8e1082e7618ff41f4075bde
xzvf <xzvf@su-root.cc>
2019-08-24 13:59:57 +0200

    Mo betta glitching

--- scripts/scrlock.sh
+++ scripts/scrlock.sh
@@ -1,4 +1,5 @@
 #/bin/sh
+# Glichting screen lock script by xzvf
 
 pngfile="/tmp/sclock.png"
 bmpfile="/tmp/sclock.bmp"
@@ -7,24 +8,23 @@ glitchedfile="/tmp/sclock_g.bmp"
 scrot -z $pngfile
 
 # convert to bmp and pixelate
-magick convert -scale 20% -scale 500% $pngfile $bmpfile
+magick convert -rotate -90 $pngfile $bmpfile
 
 
-# Glitch it with sox FROM: https://maryknize.com/blog/glitch_art_with_sox_imagemagick_and_vim/
-sox -t ul -c 1 -r 48k $bmpfile -t ul $glitchedfile trim 0 100s : echo 0.9 0.9 15 0.9
+for a in {1,2,4,5,10}
+do
+    # Glitch it with sox FROM: https://maryknize.com/blog/glitch_art_with_sox_imagemagick_and_vim/
+    sox -t ul -c 1 -r 48k $bmpfile -t ul $glitchedfile trim 0 90s : echo 1 1 $((a*2)) 0.1
+    # Rotate it by 90 degrees
+    magick convert -scale $((100/(a)))% -scale $((100*(a)))% -rotate 90 $glitchedfile $bmpfile
+done
 
-# Rotate it by 90 degrees
-magick convert -rotate 90 $glitchedfile $bmpfile
-
-#Glitch it again and rotate it back
-sox -t ul -c 1 -r 48k $bmpfile -t ul $glitchedfile trim 0 90s : echo 0.9 0.9 15 1
-magick convert -rotate -90 $glitchedfile $glitchedfile
 
 # Add lock icon, pixelate and convert back to png
 magick convert -gravity center -font "Hack-Bold-Nerd-Font-Complete-Mono" \
-    -pointsize 200 -draw "text 0,240 ''" -channel RGBA -fill '#bf616a' \
-    $glitchedfile $pngfile
+    -pointsize 200 -draw "text 0,0 ''" -channel RGBA -fill '#bf616a' \
+    $bmpfile $pngfile
 
-i3lock -i $pngfile
+i3lock -e -u -i $pngfile
 #feh $pngfile
 rm $pngfile $bmpfile $glitchedfile
