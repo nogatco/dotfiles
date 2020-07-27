0f9dc1467953ba2116819ae181bd225bbd096d5f
xzvf <xzvf@su-root.cc>
2020-07-27 22:33:34 +0200

    Added stuff to $PATH

--- scripts/shell/exports.sh
+++ scripts/shell/exports.sh
@@ -1,6 +1,6 @@
 # exports, loaded by .zshrc
 
-export PATH="$PATH:/bin:/home/xzvf/.local/bin:/home/xzvf/.gem/ruby/2.6.0/bin:/home/xzvf/inst/android-sdk/platform-tools/:/home/xzvf/.local/bin/flutter/bin:/home/xzvf/.gem/ruby/2.7.0/bin:/home/xzvf/.cargo/bin"
+export PATH="$PATH:/bin:/home/xzvf/.local/bin:/home/xzvf/.gem/ruby/2.6.0/bin:/home/xzvf/inst/android-sdk/platform-tools/:/home/xzvf/.local/bin/flutter/bin:/home/xzvf/.gem/ruby/2.7.0/bin:/home/xzvf/.cargo/bin:/var/lib/snapd/snap/bin"
 export ANDROID_HOME="$HOME/inst/android-sdk"
 export BROWSER="/usr/bin/firefox-developer-edition"
 export EDITOR='nvim'
